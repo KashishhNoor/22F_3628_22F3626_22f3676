8ef90ce43f2b68f757587566928d284c150d6bd9
Marwa Najeeb <f223676@cfd.nu.edu.pk>
2024-10-10 10:23:53 +0500

    Added Multiplication Method

--- src/task1/calculator.java
+++ src/task1/calculator.java
@@ -10,6 +10,10 @@ public class calculator {
 	{
 		return num1+num2;
 	}
+	public double multiplication (double num1, double num2)
+	{
+		return num1*num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
